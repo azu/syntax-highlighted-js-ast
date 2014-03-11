ebbfa03173cfc7444198fddcb129ecdc812c52e5
azu <azuciao@gmail.com>
2014-03-11 19:55:26 +0900

    feat: implement generate DOM Nodes from JS AST and token.

--- index.js
+++ index.js
@@ -7,40 +7,77 @@ var rocambole = require("rocambole");
 var estraverse = require("estraverse");
 var esprima = require("esprima");
 var fs = require("fs");
-function synx(src) {
+// return matched nodes in nodeRangeList
+function nodeStartToken(nodeRangeList, token) {
+    var tokenStart = token.range[0];
+    var result = nodeRangeList.filter(function (obj) {
+        return obj.range[0] === tokenStart;
+    });
+    return result.map(function (node) {
+        return "<" + node.type + ">";
+    });
+}
+
+function nodeEndToken(nodeRangeList, token) {
+    var tokenEnd = token.range[1];
+    var result = nodeRangeList.filter(function (obj) {
+        return obj.range[1] === tokenEnd;
+    });
+    return result.map(function (node) {
+        return "</" + node.type + ">";
+    });
+}
+
+
+function tokenElement(token) {
+    return token.value;
+}
+function genSyntaxHTML(src) {
     var ast = rocambole.parse(src);
-    var token = ast.startToken;
+    // 一番depthが深い所から上と下に分けて探索する
+    var deeperNode;
+    // nodeのtypeとrangeのリストを作成
+    var nodeRangeList = [];
+
+    rocambole.moonwalk(ast, function (node) {
+        nodeRangeList.push({
+            type: node.type,
+            range: node.range
+        });
+        if (deeperNode) {
+            return;
+        }
+        deeperNode = node;
+    });
+
     var html = [];
+    // ↑ seeking
+    var token = deeperNode.startToken;
+    while (token !== ast.startToken.prev) {
+        nodeEndToken(nodeRangeList, token).forEach(function (matchNode) {
+            html.unshift(matchNode);
+        });
+        html.unshift(tokenElement(token));
+        nodeStartToken(nodeRangeList, token).forEach(function (matchNode) {
+            html.unshift(matchNode);
+        });
+        token = token.prev;
+    }
+    // ↓ seeking
+    token = deeperNode.startToken.next;
     while (token !== ast.endToken.next) {
-        var element = tokenElement(token);
-        html.push(element);
+        nodeStartToken(nodeRangeList, token).forEach(function (matchNode) {
+            html.push(matchNode);
+        });
+        html.push(tokenElement(token));
+        nodeEndToken(nodeRangeList, token).forEach(function (matchNode) {
+            html.push(matchNode);
+        });
         token = token.next;
     }
     return html;
 }
-function synxB(src) {
-    var html = [];
-    var depth = 0;
-    var ast = esprima.parse(src, {range: true});
 
-    estraverse.traverse(ast, {
-        enter: function (node, parent) {
-            var code;
-            if(node.expression) {
-                code =  (node.range) ? src.substring(node.range[0], node.range[1]) : "";
-            }
-            html.push("<" + node.type + ">" + code);
-        },
-        leave: function (node, parent) {
-            html.push("</" + node.type + ">");
-        }
-    });
-    return html;
-}
-
-function tokenElement(token) {
-    return "<" + token.type + ">" + token.value + "</" + token.type + ">";
-}
 var code = fs.readFileSync(__dirname + "/example/index.js", "utf-8");
-var results = synx(code);
-console.log(results.join(""));
+console.log(genSyntaxHTML(code).join(""));
+module.exports = genSyntaxHTML;
